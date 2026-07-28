6b9269cc3372462a0bc5fb1a1b92f3b4805a5842
Arthur Guihaire <arthur.guihaire@gmail.com>
2026-07-29 00:00:03 +0200

    debug attempt 1

--- src/client/listen.rs
+++ src/client/listen.rs
@@ -1,9 +1,9 @@
 use std::{io::Write, path::{Path, PathBuf}, str::FromStr, time::Duration};
-use crate::constants::{DATA_DIR, CHUNK_SIZE};
-use iroh::endpoint::{ConnectionError, RecvStream};
+use crate::constants::{DATA_DIR};
+use iroh::endpoint::{ConnectionError, RecvStream, SendStream};
 use n0_error::{Result, StackErrorExt, StdResultExt};
 use pigeon::FileHeader;
-use tokio::{io::{AsyncWriteExt, BufWriter}, fs::File};
+use tokio::fs::File;
 
 use crate::common::{bind_endpoint, load_or_create_identity};
 
@@ -18,7 +18,7 @@ async fn confirm_overwrite(file_path: &Path) -> bool {
     else { false }
 }
 
-async fn recv_file_chunks(recv_stream: &mut RecvStream, file_path: &Path, expected_size: u64) -> Result<()> {
+async fn recv_file_chunks(recv_stream: &mut RecvStream, send_stream: &mut SendStream, file_path: &Path, expected_size: u64) -> Result<()> {
     let result = File::create_new(file_path).await;
     let mut file = match result {
         Ok(file) => file,
@@ -28,10 +28,13 @@ async fn recv_file_chunks(recv_stream: &mut RecvStream, file_path: &Path, expect
                 File::create(file_path).await.expect("Failed to create file")
             }
             else {
-                return Err(format!("Not permitted to overwrite {}, exiting", file_path.display()).into())
+                send_stream.write_all(&[0]);
+                return Err(format!("Not permitted to overwrite {}, exiting", file_path.display()).into());
             }
         }
     };
+
+    send_stream.write_all(&[1]);
 
     let bytes_copied = tokio::io::copy(recv_stream, &mut file).await;
     match bytes_copied {
@@ -46,31 +49,6 @@ async fn recv_file_chunks(recv_stream: &mut RecvStream, file_path: &Path, expect
         }
         Err(e) => Err(e.into_any())
     }
-
-    // let mut writer = BufWriter::new(&mut file);
-
-    // let mut buffer = vec![0u8; CHUNK_SIZE];
-    // let mut current_recv: u64 = 0;
-    // loop {
-    //     let size_read = recv_stream.read(&mut buffer).await.anyerr()?;
-    //     match size_read {
-    //         None => {
-    //             if current_recv < header.size {
-    //                 eprintln!("EOF reached early, potentially corrupt file");
-    //             }
-    //             break;
-    //         },
-    //         Some(0) => continue, // received 0 bytes, this shouldn't happen in theory
-    //         Some(size) => {
-    //             current_recv += size as u64;
-    //             if current_recv > header.size {
-    //                 eprintln!("something went wrong, extra bytes were sent");
-    //                 break;
-    //             }
-    //             writer.write_all(&buffer[..size]).await.anyerr()?;
-    //         }
-    //     }
-    // }
 }
 
 pub async fn listen() -> Result<()> {
@@ -92,13 +70,12 @@ pub async fn listen() -> Result<()> {
         let remote_id = conn.remote_id();
 
         tokio::spawn(async move {
-            let (_, mut recv) = conn.accept_bi().await.anyerr()?;
+            let (mut send, mut recv) = conn.accept_bi().await.anyerr()?;
 
             // let message = recv.read_to_end(4096).await.anyerr()?;
             // let message = String::from_utf8(message).anyerr()?;
             let header = recv.read_chunk(size_of::<FileHeader>()).await.anyerr()?.unwrap();
             let header: FileHeader = postcard::from_bytes(&header).anyerr()?;
-            println!("received from {remote_id}: {}", header.filename);
 
             let _ = recv_file_chunks(&mut recv, &PathBuf::from_str(&header.filename).unwrap(), header.size).await;
 
